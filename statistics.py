b5b258418a7fd0b899b56834608d4d9ebb677dcd
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-05-23 13:10:33 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,5 +1,7 @@
+from statistics import mean
+
 def calculateStats(numbers):
-  avg = mean(numbers)
-  max = max(numbers)
-  min = min(numbers)
-  return avg, min, max
+  avgVal = mean(numbers)
+  maxVal = max(numbers)
+  minVal = min(numbers)
+  return avgVal, minVal, maxVal
