10d59c0024f31da12b217b8d07ba2360a035b24e
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-05-26 13:26:01 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,11 +1,11 @@
 def calculateStats(numbers):
   if numbers == 0:
-    avgVal = 'nan'
-    minVal = 'nan'
-    maxVal = 'nan'
+    avgVal = "nan"
+    minVal = "nan"
+    maxVal = "nan"
+  return avgVal,minVal,maxVal
 #   if numbers != 0:
 #     l = len(numbers)
 #     avgVal = sum(numbers) / l
 #     maxVal = max(numbers)
 #     minVal = min(numbers)
-  return avgVal,minVal,maxVal
