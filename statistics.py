355e4e1a94f38d36c2c887dc76a25d34717f5262
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-05-26 16:25:22 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,27 +1,36 @@
 import math as m
 
 def calculateStats(numbers):
-  l = len(numbers)
-  avgVal = sum(numbers) / l
-  maxVal = max(numbers)
-  minVal = min(numbers)
-  return avgVal,minVal,maxVal
+  if m.isnan(numbers):
+    l = len(numbers)
+    for i in range(l):
+      value = m.isnan(i)
+      return value
+  else:
+    l = len(numbers)
+    avgVal = sum(numbers) / l
+    maxVal = max(numbers)
+    minVal = min(numbers)
+    return avgVal,minVal,maxVal
 
-def checkNumeric(numbers):
-  l = len(numbers)
-  for i in range(l):
-    value = m.isnan(i)
-    return value
+# def checkNumeric(numbers):
+#   l = len(numbers)
+#   for i in range(l):
+#     value = m.isnan(i)
+#     return value
 
-# def EmailAlert():
-#   i = 0
-#   i += 1
-#   return i
+def EmailAlert(e):
+  i = 0
+  if 
+  i += 1
+  return i
 
-# def LEDAlert():
-#   return 3
+def LEDAlert(j):
+  
 
-# def StatsAlerter(Thres, Alert):
-#   def checkAndAlert(numbers):
+def StatsAlerter(Thres, Alert):
+  def checkAndAlert(numbers):
+    if numbers > Thres:
+      
     
   
