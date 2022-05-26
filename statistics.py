f89015c4f4d5fa75a253bd3c64d5b530a554e696
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-05-26 13:46:06 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,11 +1,17 @@
 import math
 
 def calculateStats(numbers):
-  if bool(numbers):
-    return math.nan
-#   if numbers != 0:
-#     l = len(numbers)
-#     avgVal = sum(numbers) / l
-#     maxVal = max(numbers)
-#     minVal = min(numbers)
-#   return avgVal,minVal,maxVal
+#   if bool(numbers):
+#     return math.nan
+  if numbers != 0:
+    l = len(numbers)
+    avgVal = sum(numbers) / l
+    maxVal = max(numbers)
+    minVal = min(numbers)
+  return avgVal,minVal,maxVal
+
+# def StatsAlerter(Threshold, Alert):
+  
+
+# def checkAndAlert(numbers):
+  
