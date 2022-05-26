fc34f24e68f50c23780329a7179e7492ae68027d
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-05-26 14:36:56 +0530

    Update statistics.test.py

--- statistics.test.py
+++ statistics.test.py
@@ -11,9 +11,8 @@ class StatsTest(unittest.TestCase):
     self.assertAlmostEqual(computedStats[1], 1.5, delta=epsilon)
 
   def test_avg_is_nan_for_empty_input(self):
-    computedStats = statistics.calculateStats(['test','bosch','python'])
-    if computedStats != 0: 
-      self.assertAlmostEqual(m.isnan(computedStats), True)
+    computedStats = statistics.calculateStats(['test','bosch','python']
+    self.assertEqual(computedStats, True)
     # All fields of computedStats (average, max, min) must be
     # nan (not-a-number), as defined in the math package
     # Design the assert here.
